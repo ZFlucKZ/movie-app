bd7d1a87aebc769d2dc5a2ff488bd02aad0e67ef
Fluke <guyguy5050@gmail.com>
2022-10-05 17:58:25 +0700

    Create Components

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {data} from '../data';
+import MovieCard from './MovieCard'
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='main'>
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favorites</div>
+        </div>
+
+        <div className='list'>
+          {data.map((movie, index) => (
+            <MovieCard movie={movie} key={`movie-${index}`}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
